d9b60129ea0853028538c793ab288a267e545d42
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-11 13:44:47 +0500

    complete rot dot white component

---
+++ src/components/RotDotWhite/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./index.module.scss";
+import rotImg1 from "../../images/growRotImgDotWhite.png";
+
+const RotDot = ({ stylesfromProps1, stylesfromProps2 }) => {
+  return (
+    <div className={styles.rotImgWrapper} style={stylesfromProps1}>
+      <img
+        className="img-fluid"
+        src={rotImg1}
+        alt="rotating img"
+        style={stylesfromProps2}
+      />
+    </div>
+  );
+};
+
+export default RotDot;
