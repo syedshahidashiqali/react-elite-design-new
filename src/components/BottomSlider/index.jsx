e2911e4606d2f53195c897ff5a4b27d5ec166a3d
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-12 15:18:25 +0500

    slider commponent responsiveness

--- src/components/BottomSlider/index.jsx
+++ src/components/BottomSlider/index.jsx
@@ -7,6 +7,8 @@ import img3 from "../../images/sliderImg3.png";
 import img4 from "../../images/sliderImg4.png";
 import img5 from "../../images/sliderImg5.png";
 import img6 from "../../images/sliderImg6.png";
+import prev from "../../images/arrow-prev.png";
+import next from "../../images/arrow-next.png";
 
 const imgs = [img1, img2, img3, img4, img5, img6];
 
@@ -26,29 +28,80 @@ function BotSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    padding: 10,
+    // nextArrow: <SampleNextArrow />,
+    // prevArrow: <SamplePrevArrow />,
   };
   return (
     <div className="botSliderMainWrapper">
       <Container>
         <Row>
-          <Col md={12}>
+          <Col md={12} sm={12} xs={12}>
             <div className="sliderMainWrapper">
               <Slider {...settings}>
-                {[0, 1, 2, 3, 4, 5].map((item, i) => (
-                  <div className="sliderContent" key={i}>
-                    <h4>
-                      Andrew Garcia
-                      <strong>Brand Manager</strong>
-                    </h4>
-                    <p>
-                      Elite Design Hub did a wonderful job designing and
-                      developing a customized website for us. They infused
-                      arresting visual elements that capture visitors’ attention
-                      right away. Couldn’t ask for a better team to work with.
-                    </p>
-                  </div>
-                ))}
+                <div className="sliderContent">
+                  <h4>
+                    Andrew Garcia
+                    <strong>Brand Manager</strong>
+                  </h4>
+                  <p>
+                    Elite Design Hub did a wonderful job designing and
+                    developing a customized website for us. They infused
+                    arresting visual elements that capture visitors’ attention
+                    right away. Couldn’t ask for a better team to work with.
+                  </p>
+                </div>
+                <div className="sliderContent">
+                  <h4>
+                    Johnathan Smith
+                    <strong>Entrepreneur</strong>
+                  </h4>
+                  <p>
+                    Excellent video animation services. The folks created a
+                    striking video that conveyed our message in an engaging way.
+                    The customer support team was approachable as well. Highly
+                    recommended!
+                  </p>
+                </div>
+                <div className="sliderContent">
+                  <h4>
+                    Hernandez
+                    <strong>Businessperson</strong>
+                  </h4>
+                  <p>
+                    Literally, I was blown away by Elite Design Hub’s expertise
+                    in designing logos. Though I had a raw concept in my mind,
+                    they refined it far beyond my expectations. Simply amazing.
+                    Keep nailing it guys.
+                  </p>
+                </div>
+                <div className="sliderContent">
+                  <h4>Exceptional Work by the Logo Inifinix Team</h4>
+                  <p>
+                    We hired Elite Design Hub to boost your SEO. They kept us
+                    posted about every single development through their
+                    streamlined communication. 2 months down the line, our
+                    search engine ranking improved big time and we started
+                    getting an abundance of traffic.
+                  </p>
+                </div>
+                <div className="sliderContent">
+                  <h4>A wonderful experience and great service</h4>
+                  <p>
+                    After getting our website completely redesigned by these
+                    folks, its functionality increased dramatically. It does not
+                    take much to maintain our website now. Thanks, Elite Design
+                    Hub.
+                  </p>
+                </div>
+                <div className="sliderContent">
+                  <h4>Truly remarkable!</h4>
+                  <p>
+                    Working with Elite Design Hub was an absolute pleasure. They
+                    were way more superior than other agencies we had worked
+                    with in the past. My real estate website is now appearing on
+                    the 1st page of Google in local searches.
+                  </p>
+                </div>
               </Slider>
             </div>
           </Col>
@@ -59,3 +112,19 @@ function BotSlider() {
 }
 
 export default BotSlider;
+
+function SampleNextArrow() {
+  return (
+    <div>
+      <img className="img-fluid" src={next} alt="" />
+    </div>
+  );
+}
+
+function SamplePrevArrow() {
+  return (
+    <div>
+      <img className="img-fluid" src={prev} alt="" />
+    </div>
+  );
+}
